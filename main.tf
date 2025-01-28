448eea9fc6a5a8064dc608efdd869339431d9f34
Ihor Hartmann <ihorhartmann@gmail.com>
2025-01-28 13:28:50 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+resource "docker_image" "nginx_image" {
+  name = "nginx:latest"
+}
+
+variable "volume_dir" {
+  type    = string
+  default = "C:\\3.1.1 terraform hw"
+}
+
+resource "docker_container" "nginx_hello_world" {
+  name  = "nginx_hello_world"
+  image = docker_image.nginx_image.name
+  depends_on = [local_file.index_html]
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+
+  volumes {
+    host_path      = "${var.volume_dir}\\html"
+    container_path = "/usr/share/nginx/html"
+  }
+}
+
+# html folder creation
+resource "null_resource" "create_volume_dir" {
+  provisioner "local-exec" {
+    command = "cmd.exe /c mkdir ${var.volume_dir}\\html"
+  }
+}
+
+# html page creation
+resource "local_file" "index_html" {
+  filename = "${var.volume_dir}\\html\\index.html"
+  content  = "<html><body><h1>Hello, World!</h1></body></html>"
+  depends_on = [null_resource.create_volume_dir]
+}

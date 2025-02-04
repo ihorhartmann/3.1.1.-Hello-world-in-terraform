6ff1096a1553d7de1932b987d4c93998b8de19d9
Ihor Hartmann <ihorhartmann@gmail.com>
2025-02-04 14:28:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,41 +7,15 @@ terraform {
   }
 }
 
-resource "docker_image" "nginx_image" {
-  name = "nginx:latest"
+module "terraform_hw" {
+  source      = "./modules/terraform_hw"
+  volume_dir  = var.volume_dir
+  container_name = "nginx_hello_world"
+  external_port = 8080
+  internal_port = 80
 }
 
 variable "volume_dir" {
   type    = string
   default = "/tmp/terraform_hw"
-}
-
-resource "docker_container" "nginx_hello_world" {
-  name  = "nginx_hello_world"
-  image = docker_image.nginx_image.name
-  depends_on = [local_file.index_html]
-
-  ports {
-    internal = 80
-    external = 8080
-  }
-
-  volumes {
-    host_path      = "${var.volume_dir}/html"
-    container_path = "/usr/share/nginx/html"
-  }
-}
-
-# html folder creation
-resource "null_resource" "create_volume_dir" {
-  provisioner "local-exec" {
-    command = "mkdir -p ${var.volume_dir}/html"
-  }
-}
-
-# html page creation
-resource "local_file" "index_html" {
-  filename = "${var.volume_dir}/html/index.html"
-  content  = "<html><body><h1>Hello, World!</h1></body></html>"
-  depends_on = [null_resource.create_volume_dir]
 }

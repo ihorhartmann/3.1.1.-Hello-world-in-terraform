814af90f8c37ef66b45a90a48fc76ab98b7d9ab1
Ihor Hartmann <ihorhartmann@gmail.com>
2025-02-04 22:02:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,13 +10,8 @@ terraform {
 
 module "terraform_hw" {
   source         = "./modules/terraform_hw"
-  volume_dir     = var.volume_dir
+  volume_dir     = "/tmp/terraform_hw"
   container_name = "nginx_hello_world"
   external_port  = 8080
   internal_port  = 80
-}
-
-variable "volume_dir" {
-  type    = string
-  default = "/tmp/terraform_hw"
 }

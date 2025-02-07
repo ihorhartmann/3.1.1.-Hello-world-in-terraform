75d167e188b58650708bf82f5a47dcbfd32faf8c
Ihor <ihorhartmann@gmail.com>
2025-02-07 20:43:17 +0200

    Revert "added dependencies between modules"
    This reverts commit 49b05f3cabcf4263896b101433a800492bf99416.

--- modules/nginx/main.tf
+++ modules/nginx/main.tf
@@ -5,6 +5,7 @@ resource "docker_image" "nginx_image" {
 resource "docker_container" "nginx_container" {
   name  = "nginx_container"
   image = docker_image.nginx_image.name
+  restart = "on-failure"
 
   networks_advanced {
     name = var.network_name

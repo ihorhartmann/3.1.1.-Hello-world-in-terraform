49b05f3cabcf4263896b101433a800492bf99416
Ihor <ihorhartmann@gmail.com>
2025-02-07 20:34:07 +0200

    added dependencies between modules

--- main.tf
+++ main.tf
@@ -5,19 +5,23 @@ module "todo_app_network" {
 module "backend" {
   source       = "./modules/backend"
   network_name = module.todo_app_network.network_name
+  depends_on   = [module.todo_app_network, module.mongodb]
 }
 
 module "frontend" {
   source       = "./modules/frontend"
   network_name = module.todo_app_network.network_name
+  depends_on   = [module.todo_app_network, module.backend]
 }
 
 module "mongodb" {
   source       = "./modules/mongodb"
   network_name = module.todo_app_network.network_name
+  depends_on   = [module.todo_app_network]
 }
 
 module "nginx" {
   source       = "./modules/nginx"
   network_name = module.todo_app_network.network_name
+  depends_on   = [module.todo_app_network, module.backend, module.frontend]
 }
